0ac7866d10fed02d9b8fabad0c42249737729cd5
byhaqker <119764335+byhaqker@users.noreply.github.com>
2022-12-04 16:35:42 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Sabia que me escojerias<3')
+    alert('Sabia que me escojerias<3, en serio te debo mucho')
 });
 
 const noBtn = document.querySelector('#noBtn');
